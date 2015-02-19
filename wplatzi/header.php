6a59efc177c2a3f12973829d56afc10b7dcf0b08
Diego B. Correa <ddumst@gmail.com>
2015-02-19 10:56:51 -0500

    Add comments to functions

--- wplatzi/header.php
+++ wplatzi/header.php
@@ -3,6 +3,10 @@
 <head>
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+	<?php 
+		// Forma de uso 
+		//wp_title( $sep, $display, $seplocation ); 
+	?>
 	<title><?php wp_title( '|', true, 'right' ); ?></title>
 	<?php 
 		// Obtener la primera parte del contenido
